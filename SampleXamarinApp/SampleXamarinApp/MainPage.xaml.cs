a09bac7a9748b1c586750ec0027dc68cf3d264dc
Erick Kurniawan <erick.kurniawan@gmail.com>
2020-07-01 16:09:53 +0700

    Add project files.

---
+++ SampleXamarinApp/SampleXamarinApp/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace SampleXamarinApp
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            //btnSubmit.Clicked += btnSubmit_Clicked;
+        }
+
+        private void btnSubmit_Clicked(object sender, EventArgs e)
+        {
+            var firstname = entryUsername.Text;
+            var lastname = "Kurniawan";
+            lblResult.Text = "Nama anda : " + firstname + " " + lastname ;
+            DisplayAlert("Keterangan", $"Nama anda: {firstname} {lastname}", "OK");
+        }
+    }
+}
